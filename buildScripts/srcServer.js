c3c29bee45d489028a013d427e9d11e45bd3b45a
Mike Phelps <Mike Phelps>
2016-12-14 09:39:40 -0500

    added in Babel Transpiler

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 var _dirname = "D:/Development/pluralsight-js-dev-env/";
 
 
 /*eslint-disable no-console */
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
 
 
 
