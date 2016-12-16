030d5e36dfa02242d4cec337d50a95524a949990
Mike Phelps <Mike Phelps>
2016-12-16 14:25:37 -0500

    Configured webpack bundler

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 var _dirname = "D:/Development/pluralsight-js-dev-env/";
 
 
 /*eslint-disable no-console */
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
 
-
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo:true,
+    publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res){
     res.sendFile(path.join(_dirname, 'src/index.html'));
